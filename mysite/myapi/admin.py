93fb3500f1f626afaa7ee21de6e8669b9c12c273
Amr El-basuony <45123130+Hiden-Evil@users.noreply.github.com>
2021-05-17 20:26:33 +0200

    Add files via upload

---
+++ mysite/myapi/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Employee, ProfessionalSkill, Education, Course, Experience, Organization, SocialLink, Application, Test, Question
+
+# Register your models here.
+admin.site.register(Employee)
+admin.site.register(ProfessionalSkill)
+admin.site.register(Education)
+admin.site.register(Course)
+admin.site.register(Experience)
+admin.site.register(Organization)
+admin.site.register(SocialLink)
+admin.site.register(Application)
+admin.site.register(Test)
+admin.site.register(Question)
