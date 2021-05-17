f2610e19e187683ff58be576f055a479e91e912a
Amr El-basuony <45123130+Hiden-Evil@users.noreply.github.com>
2021-05-17 20:25:59 +0200

    Delete mysite/myapi directory

--- mysite/myapi/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = User
-		fields = ('id', 'first_name', 'middle_name', 'last_name','email','summary','address','gender','marital_status','phone','country','city','cv','nationality','employment')
